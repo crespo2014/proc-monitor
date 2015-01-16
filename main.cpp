042815d3c9f4b9327f9353e77c9d14f07d2c9dc5
Lester <lester.crespo.es@gmail.com>
2015-01-16 01:44:58 +0000

    cleaned

--- main.cpp
+++ main.cpp
@@ -91,22 +91,4 @@ int main()
             p->load();
         }
     }
-
-//    char f[1024];
-//
-//    auto fd = ::open("/proc/self/status", O_RDONLY);
-//
-//    auto len = ::read(fd, f, sizeof(f));
-//    f[len] = 0;
-//    close(fd);
-//
-//    strRowColumnSplit<15, 2> rc_str(f, ':', '\n');
-//
-//    strRowSplit<3> str('.', '\n');
-//    str.reset(f);
-//    while (str.next())
-//    {
-//        std::cout << str.get(0) << std::endl;
-//        std::cout << str.getRemainder() << std::endl;
-//    }
 }
